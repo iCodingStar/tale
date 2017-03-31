793eb66f39ee5a80744ac19cdd032ab9c1dc0cce
iCodingStar <shixing.cs@gmail.com>
2017-03-31 21:04:19 +0800

    添加自己的主题：codingstar

---
+++ src/main/java/Test.java
@@ -0,0 +1,16 @@
+import javax.print.DocFlavor;
+
+/**
+ * Created by Star on 2017/3/31.
+ */
+public class Test {
+
+    public static void $(String s){
+        System.out.printf("Hello , " + s);
+    }
+
+    public static void main(String[] args) {
+        $("iCodingStar");
+    }
+
+}
